a4649b6cc929cc711a26a290b02c80051485d1ac
Vlad <7m.vladislav@gmail.com>
2022-04-08 23:44:25 +0600

    добавление и редактирование категорий

--- resources/views/admin/includes/sidebar.blade.php
+++ resources/views/admin/includes/sidebar.blade.php
@@ -10,7 +10,7 @@
         <ul class="pt-3 nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
 
             <li class="nav-item">
-                <a href="{{route('admin.category.create')}}" class="nav-link">
+                <a href="{{route('admin.category.index')}}" class="nav-link">
                     <i class="nav-icon fas fa-th-list"></i>
                     <p>
                         Категория
