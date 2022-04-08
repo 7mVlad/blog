b99da6aba6911093c3250d24a73673bd6c0801b5
7mVlad <7m.vladislav@gmail.com>
2022-04-08 15:10:02 +0600

    админка

---
+++ resources/views/admin/includes/sidebar.blade.php
@@ -0,0 +1,24 @@
+<!-- Main Sidebar Container -->
+<aside class="main-sidebar sidebar-dark-primary elevation-4">
+    <!-- Brand Logo -->
+    <a href="#" class="brand-link">
+        <span class="text-center brand-link">Kino CMS</span>
+    </a>
+
+    <!-- Sidebar -->
+    <div class="sidebar">
+        <ul class="pt-3 nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+
+            <li class="nav-item">
+                <a href="{{route('admin.category.create')}}" class="nav-link">
+                    <i class="nav-icon fas fa-th-list"></i>
+                    <p>
+                        Категория
+                    </p>
+                </a>
+            </li>
+
+        </ul>
+    </div>
+    <!-- /.sidebar -->
+</aside>
